215f18d40b9dfa82977a8546a229caa0b077eaf0
YooShijin <a7mitk000@gmail.com>
2024-07-29 02:47:28 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("");
+  const onclick = async () => {
+    let [tab] = await chrome.tabs.query({ active: true });
+    chrome.scripting.executeScript<string[], void>({
+      target: { tabId: tab.id! },
+      args: [color],
+      func: (color) => {
+        document.body.style.backgroundColor = color;
+      },
+    });
+  };
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Background Picker</h1>
+      <div className="card">
+        <input
+          type="color"
+          onChange={(e) => setColor(e.currentTarget.value)}
+          value={color}
+        ></input>
+        <button onClick={onclick}>Click Me</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
